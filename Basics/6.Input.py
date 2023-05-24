e32d556f3aede4529ab8ef5d98224976940cc845
Saahhhiiiil <ssahillkkumarr@gmail.com>
2023-05-24 23:37:33 +0530

    PROGRAMS

---
+++ Basics/6.Input.py
@@ -0,0 +1,17 @@
+print("PROGRAM 1")
+a = input() #input ()is use dto take input from a user
+print(a)
+
+print("PROGRAM 2")
+b = input("enter name :") #string acan also be used in a print function
+print("my name is ",b)
+
+print("PROGRAM 3")
+x = input("enter first number :")
+y = input("enter second number :")
+
+print(x+y) #it will not print the sum as it will take the input of x and y as string and not as integers
+
+print("sum of x and y is : ",int(x) + int(y)) #it will print the sum of x and y as it takes the input as integers
+
+print("sum of float x and y is : ",float(x)+float(y)) #it will print the sum of float vlaues of  x and y
