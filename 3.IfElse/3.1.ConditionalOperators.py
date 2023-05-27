b4bd64ebab4a011990a208ffbbbefac3335cd679
Saahhhiiiil <ssahillkkumarr@gmail.com>
2023-05-27 17:58:55 +0530

    update

---
+++ 3.IfElse/3.1.ConditionalOperators.py
@@ -0,0 +1,16 @@
+#CONDITIONAL OPERATORS
+'''
+1. >   GREATER THAN
+2. <   LESS THAN
+3. >=  GREATER THAN EQUAL TO
+4. <=  LESS THAN EQUAL TO
+5. ==  EQUAL TO
+6. !=  NOT EQUAL TO
+'''
+a = int(input("enter number :"))
+print(18>a)
+print(18>=a)
+print(18<a)
+print(18<=a)
+print(18==a)
+print(18!=a)
